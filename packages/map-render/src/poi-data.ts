f6f55418d87aa838926ab131a42bce6d37ad29c5
Alexandre MOTTET <alexandremottet.dev@gmail.com>
2026-09-05 15:46:54 +0200

    data

--- packages/map-render/src/poi-data.ts
+++ packages/map-render/src/poi-data.ts
@@ -161,5 +161,53 @@ export const pois: PointOfInterest[] = [
     "type": "chunk",
     "x": 16,
     "y": -48
+  },
+  {
+    "title": "chunk",
+    "type": "chunk",
+    "x": 32,
+    "y": -48
+  },
+  {
+    "title": "chunk",
+    "type": "chunk",
+    "x": 48,
+    "y": -48
+  },
+  {
+    "title": "chunk",
+    "type": "chunk",
+    "x": 16,
+    "y": -64
+  },
+  {
+    "title": "chunk",
+    "type": "chunk",
+    "x": 32,
+    "y": -64
+  },
+  {
+    "title": "chunk",
+    "type": "chunk",
+    "x": 48,
+    "y": -64
+  },
+  {
+    "title": "chunk",
+    "type": "chunk",
+    "x": 16,
+    "y": 16
+  },
+  {
+    "title": "chunk",
+    "type": "chunk",
+    "x": 32,
+    "y": 16
+  },
+  {
+    "title": "chunk",
+    "type": "chunk",
+    "x": 48,
+    "y": 16
   }
 ];
